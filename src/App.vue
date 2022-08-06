e3b2d4fb0f09859e3f8cf1333bc0693eddc2bc8f
rongyueHu <1752333046@qq.com>
2022-08-06 10:26:45 +0800

    自定义指令

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    姓名<input v-focus />
+    <button @click="add" v-throttle>提交按钮</button>
+    <p>{{ count }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  created () { },
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    add () {
+      this.count++
+    }
+  },
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang='less'>
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+//自定义指令
+//v-focus
+Vue.directive('focus', {
+  //调用自定义指令的钩子函数，将被绑定的元素插入的dom中
+  inserted: function (el) {
+    //聚焦元素
+    el.focus()
+  }
+})
+//v-throttle自定义指令:表单防止重复提交
+Vue.directive('throttle', {
+  bind: (el, binding) => {
+    let throttleTime = binding.value; //节流时间
+    if (!throttleTime) { //用户若不设置节流时间，则默认节流时间为2s
+      throttleTime = 10000
+    }
+    let cbFun;
+    el.addEventListener('click', event => {
+      if (!cbFun) {//第一次执行
+        cbFun = setTimeout(() => {
+          cbFun = null;
+        }, throttleTime)
+      } else {
+        event && event.stopImmediatePropagation()
+      }
+    }, true)
+  }
+})
 new Vue({
   render: h => h(App),
 }).$mount('#app')
